ec967bc9486184ac46e6ac30775fdd9ef85ba507
Lucas <lucasdesouzapeixoto@gmail.com>
2026-09-09 22:26:12 -0300

    feat: cria estrutura inicial do projeto

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import "reflect-metadata";
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
